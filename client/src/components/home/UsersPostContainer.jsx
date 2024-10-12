35aa3d50268658d9dab4cf2e319fa5ad3434719e
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-10-12 16:50:59 +0600

    add users-post & recent job sec in home page

---
+++ client/src/components/home/UsersPostContainer.jsx
@@ -0,0 +1,73 @@
+import { UserPost } from "./UserPost";
+
+export const UsersPostContainer = () => {
+  // dummy users post
+  const userPostsData = [
+    {
+      id: 1,
+      name: "Hinata Higuyen",
+      userImg:
+        "https://www.waifu.com.mx/wp-content/uploads/elementor/thumbs/Hinata-Hyuga-Cover-q6vcrqnzrh03nyd8mmi87ai7theg8un9sf5n1fx5kg.jpg",
+      message: "Graphics Designer and Marketing Specialist",
+      time: "22 April, 3:43 AM",
+      description: `Lorem ipsum dolor sit amet consectetur. Volutpat diam egestas felis urna aliquet hendrerit egestas. Pharetra amet non congue natoque in risus sit id. Iaculis est scelerisque bibendum tellus nibh semper. Sit quis risus dolor tortor.`,
+      images: [
+        "https://www.waifu.com.mx/wp-content/uploads/elementor/thumbs/Hinata-Hyuga-Cover-q6vcrqnzrh03nyd8mmi87ai7theg8un9sf5n1fx5kg.jpg",
+        "https://staticg.sportskeeda.com/editor/2022/09/e6fd1-16626714927177-1920.jpg?w=640",
+        "https://pbs.twimg.com/media/ElhZ_U3W0CMKZ0D?format=jpg&name=large",
+      ],
+      likeCount: 152,
+      comments: 15,
+      share: 3,
+    },
+    {
+      id: 2,
+      name: "Uzumaki Naruto",
+      userImg:
+        "https://mrwallpaper.com/images/hd/anime-naruto-holding-ninja-knife-dv3cqw2dgcbwupwx.jpg",
+      message: "Graphics Designer and Marketing Specialist",
+      time: "22 April, 3:43 AM",
+      description: `Lorem ipsum dolor sit amet consectetur. Volutpat diam egestas felis urna aliquet hendrerit egestas. Pharetra amet non congue natoque in risus sit id. Iaculis est scelerisque bibendum tellus nibh semper. Sit quis risus dolor tortor.`,
+      likeCount: 152,
+      comments: 15,
+      share: 3,
+    },
+  ];
+
+  return (
+    <div className="w-full max-w-2xl users-post">
+      {userPostsData.map(
+        ({
+          id,
+          name,
+          userImg,
+          message,
+          time,
+          comments,
+          description,
+          likeCount,
+          share,
+          images,
+        }) => {
+          return (
+            <UserPost
+              key={id}
+              name={name}
+              userImg={userImg}
+              message={message}
+              time={time}
+              comments={comments}
+              description={description}
+              likeCount={likeCount}
+              share={share}
+              images={images}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+// Todos:
+// Make page responsive
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import { HomeHeader } from "../components/home/HomeHeader";
+import { UsersPostContainer } from "../components/home/UsersPostContainer";
 import { UserProfile } from "../components/home/UserProfile";
+import { RecentJobPosts } from "../components/home/RecentJobPosts";
 
 const Home = () => {
   return (
     <>
       <HomeHeader />
-      <UserProfile />
+      <main className="min-h-screen px-4 xsm:px-10 py-7 bg-[#F5F5F5]">
+        <div className="flex justify-center gap-5 mx-auto home-page-contents max-w-screen-2xl">
+          <UserProfile />
+          <UsersPostContainer />
+          <RecentJobPosts />
+        </div>
+      </main>
     </>
   );
 };
 
 export default Home;
+
+// Todos:
+// make responsive
+// make dynamic
