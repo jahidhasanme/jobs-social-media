a8da51c6b3f28e9bb9f36ae7a3bca4d818388095
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-10-14 14:16:26 +0600

    create floating message button

--- client/src/components/home/FloatingMessage.jsx
+++ client/src/components/home/FloatingMessage.jsx
@@ -1,8 +1,66 @@
+import { useState } from "react";
+
 // Icons
 import { TbMessage2 } from "react-icons/tb";
 import { UserMessage } from "../message/UserMessage";
 import { IoSearch } from "react-icons/io5";
+import { RiSoundModuleFill } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
 
 export const FloatingMessage = () => {
-  return <></>;
+  const [isFloatingMessageOpen, setIsFloatingMessageOpen] = useState(false);
+
+  return (
+    <div className="absolute top-0 flex flex-col items-end gap-6 right-6 floating-message">
+      <div
+        className={`${
+          isFloatingMessageOpen
+            ? "top-0 opacity-100 visible"
+            : "invisible opacity-0 top-5"
+        } relative ease-linear duration-200 w-full max-w-sm messages bg-[#F5F5F5] rounded shadow-[5px_10px_21px_0px_rgba(0,0,0,0.25)] min-h-[662px] max-h-[662px] scroll-bar-none overflow-y-scroll`}
+      >
+        {/* Top Area */}
+        <div className="flex justify-between items-center top-search bg-[#00B2FF] p-4 rounded-t-md rounded-tl-md">
+          <p className="font-semibold text-white">Messages</p>
+          <div className="flex items-center gap-3 text-2xl text-white icons">
+            <button className="search-btn">
+              <IoSearch />
+            </button>
+            <button className="filter-btn">
+              <RiSoundModuleFill />
+            </button>
+          </div>
+        </div>
+        {/* User Messages */}
+        <div className="messages p-2 space-y-2 bg-[#F5F5F5] rounded">
+          <UserMessage />
+          <UserMessage />
+          <UserMessage />
+          <UserMessage />
+          <UserMessage />
+        </div>
+      </div>
+      <button
+        onClick={() => setIsFloatingMessageOpen(!isFloatingMessageOpen)}
+        className={`${
+          isFloatingMessageOpen
+            ? "!bg-[#F5F5F5] text-[#535353]"
+            : "text-white bg-[#1976D2]"
+        } relative outline-none message-btn shadow-[5px_10px_21px_0px_rgba(0,0,0,0.25)] flex justify-center items-center w-14 h-14 rounded-full`}
+      >
+        {isFloatingMessageOpen ? (
+          <IoMdClose className="text-3xl" />
+        ) : (
+          <TbMessage2 className="text-4xl" />
+        )}
+        <div
+          className={`absolute -top-2 right-0 notification-count w-6 h-6 bg-[#FF3D00] rounded-full ${
+            isFloatingMessageOpen ? "hidden" : ""
+          }`}
+        >
+          <span className="">2</span>
+        </div>
+      </button>
+    </div>
+  );
 };
