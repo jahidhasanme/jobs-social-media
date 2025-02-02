477d5388c6d637e2bd0e0d3d7570d864f989b5e8
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2025-02-03 04:21:53 +0600

    feat: add validation middleware for login and signup routes

---
+++ server/src/middlewares/signupValidators.js
@@ -0,0 +1,24 @@
+import { check, validationResult } from "express-validator";
+export const doSignupValidators = [
+  check("email").isEmail().withMessage("Email is required").trim(),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
+];
+
+export const doSignupValidationHandler = function (req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    res.status(500).send({
+      data: {
+        username: req.body.username,
+      },
+      errors: mappedErrors,
+    });
+  }
+};
--- server/src/routes/signupRoute.js
+++ server/src/routes/signupRoute.js
@@ -1,8 +1,17 @@
 import express from "express";
 import signupController from "../controllers/signupController.js";
+import {
+  doSignupValidationHandler,
+  doSignupValidators,
+} from "../middlewares/signupValidators.js";
 
 const router = express.Router();
 
-router.post("/", signupController);
+router.post(
+  "/",
+  doSignupValidators,
+  doSignupValidationHandler,
+  signupController
+);
 
 export default router;
