e69fd23f9ee47801d00a868b9a35f7fc7c0ea052
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2025-02-07 23:27:01 +0600

    feat: enhance edit user functionality with authentication middleware and fixed bugs on edit user

--- server/src/routes/editUserRoute.js
+++ server/src/routes/editUserRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import editUser from "../controllers/editUserController.js";
+import { checkLogin } from "../middlewares/checkLogin.js";
 const router = express.Router();
 
-router.put("/", editUser);
+router.put("/", checkLogin, editUser);
 
 export default router;
